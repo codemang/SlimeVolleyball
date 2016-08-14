85e91ae7860431a26d4c1d983366dc4f248d640d
Dave Lee <davmlee@umich.edu>
2016-08-14 18:36:51 -0400

    organization I prefer

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ import UUID    from 'node-uuid';
 // Use app as the handler for an http rqeuest
 let app = express();
 
+// The server
+let server = http.createServer(app);
+
+// Attach socket.io to server
+let listener = io.listen(server);
+
 // Log http requests
 app.use(require('morgan')('dev'));
-
-let server = http.createServer(app).listen(1337);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/Public/index.html');
 });
 
-let listener = io.listen(server);
-
+// Listen on the 'connection' event
 listener.sockets.on('connection', (socket) => {
+	console.log("Client connected...");
+
   setInterval(() => {
     socket.emit('date', { date:  new Date()});
   }, 1000);
 });
+
+server.listen(1337);
