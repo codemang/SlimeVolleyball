2bfa781fa18c9e3f0df2590a097e200f340f0394
Nathaniel Rubin <nateboop@gmail.com>
2016-08-24 23:21:51 -0400

    Create mission control class, talks to client
    This class listens for connections from a client and tries to find a
    game/room for the client. If no game exists, it creates a new one.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import http from 'http';
 import express from 'express';
 import GameCore from '../lib/game_core';
+import MissionControl from '../lib/mission_control';
 
 const SERVER_PORT = 8080;
 
@@ -11,19 +12,18 @@ const SERVER_PORT = 8080;
 
 let app = express();
 let server = http.createServer(app);
+
 app.use('/', express.static(path.resolve(__dirname, 'build')))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/build/index.html');
 })
+
 server.listen(SERVER_PORT, () => {
    console.log(`Server is now running on http://localhost:${SERVER_PORT}`);
 });
 
 let io = require('socket.io')(server);
-//On connection by the client, create a new GameCore
-io.on('connection', (socket) => {
-    console.log('socket connected');
-    //Create GameCore which at the moment emits the time to
-    //the client every second
-    new GameCore(socket, true);
-});
+
+// Start game manager
+let mission_control = new MissionControl(io);
+mission_control.liftOff();
